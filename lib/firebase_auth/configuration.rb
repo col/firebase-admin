2e7e67e5720b4cbac4a7ef269b9150606ed71323
Colin Harris <colin@jiva.ag>
2021-03-03 10:43:04 +0800

    Update default endpoint to use https

--- lib/firebase_auth/configuration.rb
+++ lib/firebase_auth/configuration.rb
@@ -29,7 +29,7 @@ module FirebaseAuth
     # The endpoint that will be used to connect if none is set
     #
     # @note There is no reason to use any other endpoint at this time
-    DEFAULT_ENDPOINT = 'http://identitytoolkit.googleapis.com/'.freeze
+    DEFAULT_ENDPOINT = 'https://identitytoolkit.googleapis.com/'.freeze
 
     # The response format appended to the path and sent in the 'Accept' header if none is set
     #
